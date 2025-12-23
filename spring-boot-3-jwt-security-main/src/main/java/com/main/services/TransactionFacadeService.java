316f655ac934d00c453eb49ecf7ec94d8cec1055
AliAlmskee <ali.mski2001@gmail.com>
2025-12-23 12:07:44 +0300

    fix facade service injection

--- spring-boot-3-jwt-security-main/src/main/java/com/main/services/TransactionFacadeService.java
+++ spring-boot-3-jwt-security-main/src/main/java/com/main/services/TransactionFacadeService.java
@@ -2,16 +2,15 @@ package com.main.services;
 
 import com.main.facade.TransactionFacade;
 import com.main.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class TransactionFacadeService {
 
     private final TransactionFacade facade;
-
-    public TransactionFacadeService(AccountRepository repository) {
-        this.facade = new TransactionFacade(repository);
-    }
 
     public boolean transfer(Long senderId,
                             Long receiverId,
