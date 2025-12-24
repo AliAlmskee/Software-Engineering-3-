510add5f178e1bfd36dcb25b003b1f04cbc45373
AliAlmskee <ali.mski2001@gmail.com>
2025-12-24 10:21:26 +0300

    add RateLimitFilter

---
+++ spring-boot-3-jwt-security-main/src/main/java/com/main/services/RateLimitingService.java
@@ -0,0 +1,33 @@
+package com.main.services;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class RateLimitingService {
+
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String ip) {
+        return cache.computeIfAbsent(ip, this::createNewBucket);
+    }
+
+    private Bucket createNewBucket(String ip) {
+
+        // Example: 10 requests per minute
+        Bandwidth limit = Bandwidth.classic(
+                10,
+                Refill.greedy(10, Duration.ofMinutes(1))
+        );
+
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
+    }
+}
